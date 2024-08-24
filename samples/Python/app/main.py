46e000162c1bc368fd03fa1c3a6b2a28d9c2ce5f
jafta1083 <molotojafta30@gmail.com>
2024-08-24 21:10:03 +0000

    git commit --allow-empty -m 'test' git push origin master

--- samples/Python/app/main.py
+++ samples/Python/app/main.py
@@ -2,7 +2,7 @@ def main():
     print("Implement your Redis server here!")
 
     # Uncomment this to pass the first stage
-    #
+    #git commit --allow-empty -m 'test'git push origin master
     # import socket
     # s = socket.create_server(("localhost", 6379), reuse_port=True)
     # s.accept() # wait for client
